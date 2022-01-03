874b27e7fad5fd8a7fb2abad47d1940af48326d3
Danillo <danillo.targino@gmail.com>
2022-01-03 12:06:59 -0400

    upgrade do JS e introdução de variaveis

---
+++ js/introdução-variaveis.js
@@ -0,0 +1,13 @@
+// Introdução a variaveis e funções.
+
+// para variavel no js: 'const' - é uma constante, ou seja, algo que não va mudar. e 'let' - variavel mutavel. PS: Sempre que puder use 'const'
+
+//  exemplos.:
+
+let pessoa = { firstName: 'Danillo', lastName: 'Acassio'}
+
+const texto = '${pessoa.firstName} => ${pessoa.lastName}';
+
+console.log(texto);
+
+// 'console.log' - para debbugin, se usa na construção do desenvolvimento, para o usuario final não faz menor difernça!
